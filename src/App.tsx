2a4decf7886112f021685f689622a68286ff0ef6
hasankarim18 <hallysust@gmail.com>
2024-06-23 12:05:57 +0600

    react one way data flow 1

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="max-w-7xl px-0 md:px-5 mx-auto">
+        <h1 className="text-2xl">React one way data flow</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
